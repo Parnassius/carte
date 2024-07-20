ee60be279734dbbb7a1ef53fa37ab35c38a5efd3
Parnassius <Parnassius@users.noreply.github.com>
2024-07-20 15:46:15 +0200

    Add some full game tests

---
+++ tests/conftest.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+import random
+from typing import TypeVar
+
+import pytest
+import pytest_asyncio
+from aiohttp import web
+
+from carte.games import BaseGame, Briscola
+from carte.types import Player
+
+T_BaseGame = TypeVar("T_BaseGame", bound=BaseGame)
+
+
+class DummyWebsocketResponse(web.WebSocketResponse):
+    def __init__(self) -> None:
+        super().__init__()
+        self._closed = False
+        self._messages: list[str] = []
+
+    async def send_str(
+        self, data: str, compress: bool | None = None  # noqa: ARG002
+    ) -> None:
+        self._messages.append(data)
+
+    def _get_message(self, cmd: str) -> list[str]:
+        return next(x.split("|") for x in self._messages if x.startswith(f"{cmd}|"))
+
+
+Game = tuple[T_BaseGame, list[DummyWebsocketResponse]]
+
+
+def make_game(game_type: type[T_BaseGame]) -> Game[T_BaseGame]:
+    game = game_type()
+    websockets = []
+
+    for i in range(game.number_of_players):
+        player = Player(f"player{i}", f"Player {i}")
+        ws = DummyWebsocketResponse()
+        websockets.append(ws)
+        game.websockets.add(ws)
+        player.websockets.add(ws)
+        game._players.append(player)
+
+    return game, websockets
+
+
+@pytest.fixture
+def briscola() -> Game[Briscola]:
+    return make_game(Briscola)
+
+
+@pytest.fixture(autouse=True)
+def set_seed(request: pytest.FixtureRequest) -> None:
+    if (
+        isinstance(request.node, pytest_asyncio.plugin.Coroutine)
+        and request.node.callspec is not None
+    ):
+        seed = request.node.callspec.params.get("seed")
+        if isinstance(seed, int):
+            random.seed(seed)
---
+++ tests/test_briscola.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import pytest
+
+from carte.games import Briscola
+from carte.types import GameStatus
+from tests.conftest import Game
+
+
+@pytest.mark.parametrize(
+    "seed, results",
+    [
+        (123, [50, 70]),
+        (321, [60, 60]),
+        (111, [27, 93]),
+    ],
+)
+async def test_full_game(
+    briscola: Game[Briscola], seed: int, results: list[int]
+) -> None:
+    game, websockets = briscola
+
+    await game._prepare_start()
+
+    while game._game_status is GameStatus.STARTED:
+        player_id = game._current_player_id
+        card = game._players[player_id].hand[0]
+        await game.handle_cmd(websockets[player_id], "play", card.suit, card.number)
+
+    assert results == [int(x) for x in websockets[0]._get_message("results")[1:]]
